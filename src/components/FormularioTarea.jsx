03664ace7c577e6197624ab4ccbbab1027c9334e
Mauricio Aguirre <ing.mauricio.aguirre@gmail.com>
2023-07-22 19:25:51 -0300

    Agredando tareas al Array de Tareas

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -5,20 +5,32 @@ import { useState } from "react";
 const FormularioTarea = () => {
   const [tarea, setTarea] = useState(""); //State para los datos del input o form.control
   const [listaTareas, setListaTareas] = useState([]); //Array para almacenar los states tarea
+  //Funcion para controlar el evento onSubmit del Form.
+  const handleSubmit = (e) => {
+    //para evitar que recargue la pagina por cada evento submit.
+    e.preventDefault();
+    //Guardar tarea en el Array de listaTareas
+    //El operador Spred ...  me sirve para conservar los existente en el Array
+    setListaTareas([...listaTareas, tarea]);
+    //Borro el input para una nueva carga mediante setTarea porque el value del Input esta vinculado al State tarea.
+    setTarea("");
+  };
   return (
     <>
-      <Form.Group className="mb-1 d-flex" controlId="tarea">
-        <Form.Control
-          type="text"
-          placeholder="ingresa una tarea"
-          value={tarea}
-          onChange={(e) => setTarea(e.target.value)} //para guardar el value del Input dentro del State
-        />
-        <Button className="ms-2" variant="success" type="submit">
-          +
-        </Button>
-      </Form.Group>
-      <ListaTareas></ListaTareas>
+      <form onSubmit={handleSubmit}>
+        <Form.Group className="mb-1 d-flex" controlId="tarea">
+          <Form.Control
+            type="text"
+            placeholder="ingresa una tarea"
+            value={tarea}
+            onChange={(e) => setTarea(e.target.value)} //para guardar el value del Input dentro del State
+          />
+          <Button className="ms-2" variant="success" type="submit">
+            +
+          </Button>
+        </Form.Group>
+        <ListaTareas></ListaTareas>
+      </form>
     </>
   );
 };
