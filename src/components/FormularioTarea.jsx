9149ee61f68d1892455391ee262ec91020848115
Mauricio Aguirre <ing.mauricio.aguirre@gmail.com>
2023-07-22 16:09:07 -0300

    Enlazando el State tarea al value del input

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,11 +1,19 @@
 import { Form, Button } from "react-bootstrap";
 import ListaTareas from "./ListaTareas";
+import { useState } from "react";
 
 const FormularioTarea = () => {
+  const [tarea, setTarea] = useState(""); //State para los datos del input o form.control
+  const [listaTareas, setListaTareas] = useState([]); //Array para almacenar los states tarea
   return (
     <>
       <Form.Group className="mb-1 d-flex" controlId="tarea">
-        <Form.Control type="text" placeholder="ingresa una tarea" />
+        <Form.Control
+          type="text"
+          placeholder="ingresa una tarea"
+          value={tarea}
+          onChange={(e) => setTarea(e.target.value)} //para guardar el value del Input dentro del State
+        />
         <Button className="ms-2" variant="success" type="submit">
           +
         </Button>
